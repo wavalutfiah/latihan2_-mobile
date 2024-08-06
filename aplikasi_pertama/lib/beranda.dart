1aafa7c00c59b7a3781d6b004c61ab3f107de091
wavalutfiah <wavalutfiahmarhas@gmail.com>
2024-08-06 12:21:06 +0700

    commit pertama

---
+++ aplikasi_pertama/lib/beranda.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class beranda extends StatelessWidget {
+  const beranda ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Column(children: [Text("hello world")],),);
+  }
+}
---
+++ latihan2/lib/beranda.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class beranda2 extends StatelessWidget {
+  const beranda2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [Text("halo"),Text("saya"),Text("sedang"),Text("belajar"),Text("flutter")],)
+    ); 
+  }
+
+}
